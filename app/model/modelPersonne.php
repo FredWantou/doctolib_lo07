6278dd1bc95deb357d11ceef036adb36bdc14270
Briandiffo <briandiffo0123@gmail.com>
2023-05-20 20:13:51 +0200

    model/config ajout

---
+++ app/model/modelPersonne.php
@@ -0,0 +1,183 @@
+<?php
+require_once 'Model.php';
+
+class Modelproducteur {
+ private $id, $nom, $prenom, $region;
+
+ // pas possible d'avoir 2 constructeurs
+ public function __construct($id = NULL, $nom = NULL, $prenom = NULL, $region = NULL) {
+  // valeurs nulles si pas de passage de parametres
+  if (!is_null($id)) {
+   $this->id = $id;
+   $this->nom = $nom;
+   $this->prenom = $prenom;
+   $this->region = $region;
+  }
+ }
+
+ function setId($id) {
+  $this->id = $id;
+ }
+
+ function setnom($nom) {
+  $this->nom = $nom;
+ }
+
+ function setprenom($prenom) {
+  $this->prenom = $prenom;
+ }
+
+ function setregion($region) {
+  $this->region = $region;
+ }
+
+ function getId() {
+  return $this->id;
+ }
+
+ function getnom() {
+  return $this->nom;
+ }
+
+ function getprenom() {
+  return $this->prenom;
+ }
+
+ function getregion() {
+  return $this->region;
+ }
+ 
+ 
+// retourne une liste des id
+ public static function getAllId() {
+  try {
+   $database = Model::getInstance();
+   $query = "select id from producteur";
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_COLUMN, 0);
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function getMany($query) {
+  try {
+   $database = Model::getInstance();
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelVin");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function getAll() {
+  try {
+   $database = Model::getInstance();
+   $query = "select * from producteur";
+   $statement = $database->prepare($query);
+   $statement->execute();
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelProducteur");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function getOne($id) {
+  try {
+   $database = Model::getInstance();
+   $query = "select * from producteur where id = :id";
+   $statement = $database->prepare($query);
+   $statement->execute([
+     'id' => $id
+   ]);
+   $results = $statement->fetchAll(PDO::FETCH_CLASS, "ModelProducteur");
+   return $results;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return NULL;
+  }
+ }
+
+ public static function insert($nom, $prenom, $region) {
+  try {
+   $database = Model::getInstance();
+
+   // recherche de la valeur de la clé = max(id) + 1
+   $query = "select max(id) from producteur";
+   $statement = $database->query($query);
+   $tuple = $statement->fetch();
+   $id = $tuple['0'];
+   $id++;
+
+   // ajout d'un nouveau tuple;
+   $query = "insert into producteur value (:id, :nom, :prenom, :region)";
+   $statement = $database->prepare($query);
+   $statement->execute([
+     'id' => $id,
+     'nom' => $nom,
+     'prenom' => $prenom,
+     'region' => $region
+   ]);
+   return $id;
+  } catch (PDOException $e) {
+   printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+   return -1;
+  }
+ }
+ public static function ListRegion(){
+    try{
+        $database=Model::getInstance();
+        $query="select distinct region from producteur";
+        $statement=$database->prepare($query);
+        $statement->execute();
+        $results=$statement->fetchAll(PDO::FETCH_COLUMN, 0);
+        return $results;
+
+    }
+    catch(PDOException $e){
+        printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+        return NULL;
+        
+
+    }
+ }
+ public static function nbRegion(){
+    try{
+        $database=Model::getInstance();
+        $query="select region, count(*) as nombre_producteurs
+        from producteur
+        group by region;";
+        $statement=$database->query($query);
+        
+        $results=$statement->fetchAll(PDO::FETCH_ASSOC);
+        return $results;
+
+    }
+    catch(PDOException $e){
+        printf("%s - %s<p/>\n", $e->getCode(), $e->getMessage());
+        return NULL;
+        
+
+    }
+ }
+
+ public static function update() {
+  echo ("ModelProducteur : update() TODO ....");
+  return null;
+ }
+
+ public static function delete() {
+  echo ("ModelProducteur : delete() TODO ....");
+  return null;
+ }
+
+}
+?>
